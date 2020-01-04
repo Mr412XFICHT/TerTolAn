6264c3a7fab572babceb7b7c929edfadff6ab586
Mr412XFICHT <59497656+Mr412XFICHT@users.noreply.github.com>
2020-01-04 15:42:30 +0700

    Add files via upload

---
+++ ToolsMbuh.sh
@@ -0,0 +1,291 @@
+#!system/bin/sh
+clear
+# warna
+r="\033[1;31m" # merah
+g="\033[1;32m" # hijau
+y="\033[1;33m" # kuning
+b="\033[1;34m" # biru
+p="\033[1;35m" # ungu
+cy="\033[1;36m" # biru muda
+w="\033[1;37m" # putih
+echo"Tunggu Sebentar ya Asw Sekitar 5 detik"
+sleep 5
+echo $cy
+figlet "TERMUX"
+echo
+echo $r"============="$y"==================="$g"====================="
+echo $r"Author"$p"   : "$b"༆•₳ℜý࿌  ֆҜลtͥєℜֆཧᜰ꙰ꦿ➢                        "
+echo $y"Wa    "$cy"   : "$g"62859148308208                             "
+echo $g"Github"$p"   : "$y"https://github.com/Mr12XF404               "
+echo $b"System"$cy"   : "$r"Mr.124XF404"
+echo $g"============="$y"==================="$r"====================="
+echo
+echo
+sleep 5
+echo $cy"pilih menu tools nya Asw"
+echo
+echo $r"("$w"1"$r") "$r"HACK FB"
+echo $r"("$w"2"$r") "$y"SPAM SMS"
+echo $r"("$w"3"$r") "$g"DDOS LUCITA"
+echo $r"("$w"4"$r") "$r"SADAP WA"
+echo $r"("$w"5"$r") "$y"BOT CHAT WA"
+echo $r"("$w"6"$r") "$g"SPAM CALL"
+echo $r"("$w"7"$r") "$r"Hack IG Hack FB"
+echo $r"("$w"8"$r") "$y"HACK PULSA GRATIS VIA TERMUX "
+echo $r"("$w"9"$r") "$g"HACK BANK"
+echo $r"("$w"10"$r") "$r"TROJAN"
+echo $r"("$w"11"$r") "$y"INSTALL TUANB4DUT"
+echo $r"("$w"12"$r") "$g"INSTALL BAJINGANV6"
+echo $r"("$w"13"$r") "$r"VIRTEX WHATSAPP"
+echo $r"("$w"14"$r") "$y"BUAT VIRUS MEMATIKAN ANDROID"
+echo $r"("$w"15"$r") "$g"NIK & KK GRATIS"
+echo $r"("$w"16"$r") "$r"SANTET ONLINE"
+echo $r"("$w"17"$r") "$y"HACK AKUN MOBILE LEGENDS"
+echo $r"("$w"18"$r") "$g"DEFACE TEBAS INDEX"
+echo $r"("$w"19"$r") "$r"TOMBOL KANAN KIRI TERMUX"
+echo $r"("$w"20"$r") "$y"DARK FB"
+echo $r"("$w"21"$r") "$g"MEMBUAT SCRIPT DEFACE"
+echo $r"("$w"22"$r") "$r"SMS GRATIS ALL KART7U"
+echo $r"("$w"23"$r") "$y"ANIMASI KERETA API"
+echo $r"("$w"24"$r") "$g"ANIMASI CMATRIX"
+echo $r"("$w"25"$r") "$r"ANIMASI API"
+echo $r"("$w"26"$r") "$y"KUMPULAN KATA KATA BUCIN"
+echo $r"("$w"27"$r") "$g"* INSTALL BAHAN DULU COEG BIAR GAK EROR*"$y""
+read -p "PILIH TOOLS YANG ANDA BUTUHKAN AJG : " pilih;
+# batas gan
+if [ $pilih = "1" ]
+then
+    git clone https://github.com/B4N954N2-ID/DarkFb
+    cd DarkFb
+    python2 DarkFb.py
+fi
+# batas gan
+if [ $pilih = "2" ]
+then
+    git clone https://github.com/4L13199/LITESPAM
+    cd LITESPAN
+    sh LITESPAM.sh
+fi
+# batas gan
+if [ $pilih = "3" ]
+then
+    git clone https://github.com/zlucifer/lucita_ddos
+    cd lucita_ddos
+    python pukul.py
+fi
+# batas gan
+if [ $pilih = "4" ]
+then
+    git clone https://github.com/Bl4ckDr460n/HxWhatsApp
+    cd HxWhatsapp
+    python2 HxWhatsApp.py
+fi
+# batas gan
+if [ $pilih = "5" ]
+then
+    git clone https://github.com/Bl4ckDr460n/BotChat
+    cd BotChat
+    python2 Chat.py
+fi
+# batas gan
+if [ $pilih = "6" ]
+then
+    git clone https://github.com/siputra12/prank
+    cd prank
+    php call.php
+fi
+# batas gan
+if [ $pilih = "7" ]
+then
+    git clone https://github.com/MrTamfanX/TamfanFacebook
+    cd TamfanFacebook
+    chmod +x *
+    chmod -R 775 lib
+    sh install.sh
+    sh tamfan.sh
+fi
+# batas gan
+if [ $pilih = "8" ]
+then
+    git clone https://github.com/MrUncle/PulsaGratis
+    chmod +x *
+    sh pulsa.sh
+fi
+# batas gan
+if [ $pilih = "9" ]
+then
+    git clone https://github.com/MrUncle/Script-HackBank
+    cd Script-HackBank
+    chmod +x *
+    sh ALLBANK.sh
+fi
+# batas gan
+if [ $pilih = "10" ]
+then
+    git clone https://github.com/justahackers/perusak
+    cd perusak
+    python2 perusak.py
+fi
+# batas gan
+if [ $pilih = "11" ]
+then
+    git clone https://github.com/TUANB4DUT/TOOLSINSTALLERv4
+    cd TOOLSINSTALLERv4
+    chmod +x TUANB4DUT..sh
+    sh TUANB4DUT..sh
+fi
+# batas gan
+if [ $pilih = "12" ]
+then
+    git clone https://github.com/IntiTutorial/BAJINGANv6
+    cd BAJINGANv6
+    sh BAJINGAN.sh
+fi
+# batas gan
+if [ $pilih = "13" ]
+then
+    git clone https://github.com/muhammadfathul/VIRTEX
+    cd VIRTEX
+    chmod 777 virtex.sh
+    sh virtex.sh
+fi
+# batas gan
+if [ $pilih = "14" ]
+then
+    git clone https://github.com/IntiTutorial/B-VIRUS
+    cd B-VIRUS
+    python2 B-VIRUS.py
+fi
+# batas gan
+if [ $pilih = "15" ]
+then
+    git clone https://github.com/IndonesianSecurity/kkktp
+    cd kkktp
+    php kkktp.php
+fi
+# batas gan
+if [ $pilih = "16" ]
+then
+    git clone https://github.com/Gameye98/santet-online
+    cd santet-online
+    python2  santet.py
+fi
+# batas gan
+if [ $pilih = "17" ]
+then
+    git clone https://github.com/Senitopeng/PhisingGame.git
+    cd PhisingGame
+    python2 phising.py
+fi
+# batas gan
+if [ $pilih = "18" ]
+then
+    git clone https://github.com/Mr-xDODOL/Tebas
+    cd Tebas
+    sh Tebas.sh
+fi
+# batas gan
+if [ $pilih = "19" ]
+then
+    wget https://raw.githack.com/kumpul4semut/newtermux/master/key.py
+    chmod +x key.py
+    python key.py
+fi
+# batas gan
+if [ $pilih = "20" ]
+then
+    git https://github.com/B4N954N2-ID/DarkFb
+    cd DarkFb
+    python2 DarkFb.py
+fi
+# batas gan
+if [ $pilih = "21" ]
+then
+    git clone https://github.com/4L13199/LITESCRIPT
+    cd cd LITESCRIPT
+    chmod 777 LITESCRIPT.py
+    python2 LITESCRIPT.py
+fi
+# batas gan
+if [ $pilih = "22" ]
+then
+    git clone https://github.com/xractz/fsms
+    cd fsms
+    python fsms.py
+fi
+# batas gan
+if [ $pilih = "23" ]
+then
+    sl
+    sh belajar.sh
+    sl
+fi
+# batas gan
+if [ $pilih = "24" ]
+then
+    cmatrix
+    sh belajar.sh
+    cmatrix
+fi
+# batas gan
+if [ $pilih = "25" ]
+then
+    cacafire
+    sh belajar.sh
+    cacafire
+fi
+# batas gan
+if [ $pilih = "26" ]
+then
+   git clone https://github.com/MiSetya/BUCIN
+    cd BUCIN
+    sh BUCIN.sh
+fi
+# batas gan
+if [ $pilih = "27" ]
+then
+    echo "WOI NGENTOT TUNGGU SEBENTAR YA TOD LAGI INSTALL BAHAN BIAR GAK EROR BANGSD,YA WALAUPUN MENUNGGU ITU MEMBOSANKAN OKEHH.........................................................................."
+    $HOME
+    pkg update && pkg upgrade
+    pkg install python2
+    pkg install git
+    pkg install curl
+    pkg install lolcat
+    pkg install openssh
+    pkg install php
+    pkg install figlet
+    pkg install ruby
+    pkg install bash
+    pkg install sh
+    pkg install toilet
+    pkg install nano
+    pkg install nodejs
+    pkg install nodejs-lts
+    pkg install python2-dev
+    pkg install python
+    pkg install cowsay
+    pkg install tqdm
+    pkg install wc
+    pkg install apache
+    pkg install apache2
+    pkg install pip
+    pkg install wget
+    pkg install sl
+    pkg install cmatrix
+    pkg install libcaca
+    gem install lolcat
+    pip install mechanize
+    pip install requests
+    pip2 install mechanize
+    pip2 install requests
+    pip2 install progress
+    pip2 install urllib2 urllib
+    pip2 install socket
+    pip2 install -r nganunymous.txt
+    pip install -r requirements.txtpip2
+    pip2 install -r requirements.txt
+    echo "PENGINSTALLAN BAHAN SUDAH SELESAI ANJENG"
+    sh belajar.sh
+fi
+
+
